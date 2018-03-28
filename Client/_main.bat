00432da86dc36d6cb1427dcdb47254ae590461c2
MininMobile <miningamer@gmail.com>
2018-03-29 00:09:33 +0100

    Finish Client

--- Client/_main.bat
+++ Client/_main.bat
@@ -1,7 +1,22 @@
 @echo off
 call config.bat
+cd %server%
 
 :main
 cls
 echo Enter Username;
 set /p name=
+
+echo %name% has joined the chat! >> log.bat
+
+:message
+cls
+call log.bat
+set /p msg=%name%: 
+
+echo set recieved=%msg% > update.bat
+
+:wait
+if NOT exist update.bat goto wait
+
+goto message
