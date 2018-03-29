91fee509e1e844cd3567ea4a3762a365918db222
MininMobile <miningamer@gmail.com>
2018-03-29 14:54:04 +0100

    Start Client and Display

---
+++ Client/_client.bat
@@ -0,0 +1,20 @@
+@echo off
+call config.bat
+cd ..
+cd Server
+
+:main
+cls
+echo Enter Username;
+set /p name=
+
+echo set from=%name% > update.bat
+echo set recieved=Joined the chat! >> update.bat
+
+:message
+cls
+set /p msg=%name%: 
+
+echo set from=%name% > update.bat
+echo set recieved=%msg% >> update.bat
+goto message
--- Client/_main.bat
+++ Client/_main.bat
@@ -1,20 +1,2 @@
-@echo off
-call config.bat
-cd ..
-cd Server
-
-:main
-cls
-echo Enter Username;
-set /p name=
-
-echo set from=%name% > update.bat
-echo set recieved=Joined the chat! >> update.bat
-
-:message
-cls
-set /p msg=%name%: 
-
-echo set from=%name% > update.bat
-echo set recieved=%msg% >> update.bat
-goto message
+start _client.bat
+start _display.bat
