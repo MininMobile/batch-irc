488cddeea7df45a963edf2821418f75536a9819b
MininMobile <miningamer@gmail.com>
2018-03-29 14:37:15 +0100

    Add Mod Actions

--- Server/_main.bat
+++ Server/_main.bat
@@ -7,6 +7,19 @@ goto wait
 
 :update
 call update.bat
+call:checkName "%from%"
+if %mod% == 1 goto modUpdate
+goto message
+
+:modUpdate
+if "%message%" == "clear" (
+	echo echo A moderator cleared the chat! > log.bat
+	del update.bat
+	goto wait
+)
+goto message
+
+:message
 set message=%from%: %recieved%
 echo echo %message% >> log.bat
 del update.bat
